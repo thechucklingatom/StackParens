ec79364bec27daed7806523d939e4d8f34b6380d
atom <atom@atom-Linux>
2014-09-08 17:29:37 -0600

    Shell for the program.

---
+++ src/stackparens/EmptyStackException.java
@@ -0,0 +1,16 @@
+package stackparens;
+/**
+* Runtime
+*
+exception thrown when one tries to· perform operation top or
+pop on an empty stack.
+*/
+public class EmptyStackException extends RuntimeException { 
+
+    public EmptyStackException(String err) { 
+
+        super( err); 
+
+    } 
+
+}
---
+++ src/stackparens/FullStackException.java
@@ -0,0 +1,16 @@
+package stackparens;
+/**
+* Runtime
+*
+exception thrown when one tries to· perform operation top or
+pop on an empty stack.
+*/
+public class FullStackException extends RuntimeException { 
+
+    public FullStackException(String err) { 
+
+        super( err); 
+
+    } 
+
+}
